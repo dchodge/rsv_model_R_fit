075b2b8d2f5f2bcee7b656bdb9a066c752f513ee
dchodge <david_hodgson1991@hotmail.co.uk>
2021-06-30 08:03:53 +0100

    Working ptmc fit for altered uk data

--- R/plot_helpers.R
+++ R/plot_helpers.R
@@ -6,44 +6,40 @@
 # bool: flag to output the model multipled by the ascertainment probablility (epsilon). If FALSE then outputs the true predicted values.
 run_sample <- function(post, bool)
 {
-  a <- rep(NA, 25*(52*7)*1000)
-  arr <- array(a, c(52*7, 25,  1000))
-  
-  for (i in 1:1000)
-  {
+  a <- rep(NA, 25 * (52 * 7) * 1000)
+  arr <- array(a, c(52 * 7, 25,  1000))
+  for (i in 1:1000){
     j <- rdunif(1, 1, nrow(post))
-    arr[,, i] <- classEvaluateLogLikelihood$getWeeklySampleCpp(as.numeric(post[j,]), bool)
+    arr[, , i] <- classEvaluateLogLikelihood$getWeeklySampleCpp(as.numeric(post[j, ]), bool)
   }
-  out <- lapply(1:25, function(y) sapply(1:(52*7), function(x) sort(arr[x,y,])[c(25, 500 , 975)]))  
+  out <- lapply(1:25, function(y) sapply(1:(52 * 7), function(x) sort(arr[x, y, ])[c(25, 500, 975)]))
   out
 }
 
 # run_sample_annual: runs the model with 1000 random samples from the posterior, outputs the median, and 95% confidence intervals for the annual incidence per age group
 run_sample_annual <- function(post)
 {
-  a <- rep(NA, 25*1000)
+  a <- rep(NA, 25 * 1000)
   arr <- array(a, c(25,  1000))
   
-  for (i in 1:1000)
-  {
+  for (i in 1:1000) {
     j <- rdunif(1, 1, nrow(post))
     arr[, i] <- classEvaluateLogLikelihood$getAnnualIncidenceCpp(as.numeric(post[j,]))
   }
-  out <- sapply(1:25, function(s ) sort(arr[s,])[c(25, 500 , 975)])
+  out <- sapply(1:25, function(s) sort(arr[s, ])[c(25, 500, 975)])
   out
 }
 
 
 run_sample_maternal_protected <- function(post)
 {
-  a <- rep(NA, 364*1000)
+  a <- rep(NA, 364 * 1000)
   arr <- array(a, c(364,  1000))
   
-  for (i in 1:1000)
-  {
+  for (i in 1:1000) {
     j <- rdunif(1, 1, nrow(post))
-    arr[, i] <- classEvaluateLogLikelihood$getProportionBornProtectedCpp(as.numeric(post[j,]))
+    arr[, i] <- classEvaluateLogLikelihood$getProportionBornProtectedCpp(as.numeric(post[j, ]))
   }
-  out <- sapply(1:364, function(s) sort(arr[s,])[c(25, 500 , 975)])
+  out <- sapply(1:364, function(s) sort(arr[s, ])[c(25, 500, 975)])
   out
 }
